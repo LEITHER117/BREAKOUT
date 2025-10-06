cb2747801205bfc352164035b8f85b393be60df0
LEITHER117 <emasresmacias@gmail.com>
2025-10-06 14:20:30 -0600

    Practica Modulo 10

---
+++ BREAKOUT/Assets/_Scripts/Bloque Slime.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloqueSlime : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 4;
+    }
+
+    public override void RebotarBola()
+    {
+        base.RebotarBola();
+    }
+  
+    public void BolaPegajosa()
+    {
+
+    }
+}
